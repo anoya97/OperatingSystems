26601d7ef3bf4a03a1f9cbe872459567d0b9edeb
NOYA <91661771+anoya97@users.noreply.github.com>
2022-02-18 10:32:54 +0100

    Add files via upload

---
+++ p2/p2/cmake-build-debug (1)/CMakeFiles/p2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p2.dir/hist_list.c.o"
+  "CMakeFiles/p2.dir/p1.c.o"
+  "p2"
+  "p2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/p2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ p2/p2/cmake-build-debug/CMakeFiles/p2.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/armandonoya/CLionProjects/SO/p2/hist_list.c" "/home/armandonoya/CLionProjects/SO/p2/cmake-build-debug/CMakeFiles/p2.dir/hist_list.c.o"
+  "/home/armandonoya/CLionProjects/SO/p2/p1.c" "/home/armandonoya/CLionProjects/SO/p2/cmake-build-debug/CMakeFiles/p2.dir/p1.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
